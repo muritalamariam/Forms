f51b9066f54370f9e52223d802affc44779d176a
muritalamariam <muritalamariam2002@gmail.com>
2022-05-27 03:40:43 +0100

    commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form get</title>
+</head>
+<body>
+
+<form method="POST" action="user_data.php">
+
+<label for="name">
+    Name: <input type="text" name="name">
+</label> <br>
+
+<label for="name">
+    Email: <input type="email" name="Email">
+</label> <br>
+
+<label for="name">
+    Date of Birth: <input type="text" name="date of birth">
+</label> <br>
+
+<label for="name">
+    Gender: <input type="text" name="gender">
+</label> <br>
+
+<label for="name">
+    Country: <input type="text" name="country">
+</label> <br>
+
+<button type="submit">Submit</button>
+</form>
+
+
+</body>
+</html>
